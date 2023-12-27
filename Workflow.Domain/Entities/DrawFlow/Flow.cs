ef3324f722ad219d9e3811d9ee39dd018f33e174
Julio Cezar da Silva <jcezardasilva@outlook.com>
2023-12-26 22:07:33 -0300

    feat: flow metadata

--- Workflow.Domain/Entities/DrawFlow/Flow.cs
+++ Workflow.Domain/Entities/DrawFlow/Flow.cs
@@ -12,6 +12,18 @@ namespace Workflow.Domain.Entities.DrawFlow
         /// </summary>
         public Dictionary<string, FlowPage> DrawFlow { get; set; } = new Dictionary<string, FlowPage>();
         /// <summary>
+        /// The flow ID
+        /// </summary>
+        public string Id { get; set; } = string.Empty;
+        /// <summary>
+        /// The flow name
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+        /// <summary>
+        /// The flow description
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+        /// <summary>
         /// Environment variables
         /// </summary>
         public Dictionary<string, string> Environment { get; set; } = new Dictionary<string, string>();
